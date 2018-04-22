0cfe4f9cb45fec5772917ac2d20390ebe10eff72
Retkowski <fabian.retkowski@sap.com>
2018-04-22 03:02:16 +0200

    Improve results of search queries

--- backend/webserver.py
+++ backend/webserver.py
@@ -1,7 +1,7 @@
 import gensim.downloader as api
 import http.server
 import math
-import pprint
+import json
 import numpy as np
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
@@ -38,6 +38,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
         response = natural_language_understanding.analyze(
         text=t,
+        language='en',
         features=Features(
             entities=EntitiesOptions(
              sentiment=False,
@@ -50,14 +51,12 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         print(response)
 
         def cosine_measure(v1, v2):
-            print("cos-measure")
             prod = np.dot(v1, v2)
             len1 = math.sqrt(np.dot(v1, v1))
             len2 = math.sqrt(np.dot(v2, v2))
             return prod / (len1 * len2)
 
         def getVectorsOf(model, text):
-            print("getVectorsOf")
             vectors = []
             for token in wordpunct_tokenize(text):
                 try:
@@ -70,39 +69,64 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             return np.array(getVectorsOf(model, text)).mean(axis=0)
 
         query_vector = vectorize_document(t)
-        print("vectorized doc")
 
 
         # Aggregate results - NOTE redundant documents
         threshold = 0.2
-        cos_values = []
+        upper_threshold = 0.4
         out_values = []
         for document in schema.find():
             cos_value = cosine_measure(document['word2vec'], query_vector)
-            if cos_value > 0.2:
+            if cos_value > threshold:
                 for entity in response['entities']:
-                    if entity['text'] in [x[1] for x in document['entities']]:
+                    if entity['text'].lower() in [x[1].lower() for x in document['entities']]:
                         out_values.append((document, cos_value))
                 for keyword in response['keywords']:
-                    if keyword in [x[0] for x in document['keywords']] and not (document, cos_value) in out_values:
+                    if keyword["text"].lower() in [x[0].lower() for x in document['keywords']] and not (document, cos_value) in out_values:
                         out_values.append((document, cos_value))
+                if cos_value > upper_threshold and not (document, cos_value) in out_values:
+                    out_values.append((document,cos_value))
 
         res = sorted(out_values, key=lambda x: x[1])
-        print(res)
+        res = [x for x in res
+               if len(x[0]["document_title_pdf"]) > 10
+               and len(x[0]["document_title_pdf"]) < 100
+               and x[0]["document_title_pdf"][0].isalpha()
+               and len(x[0]["document_summary"]) > 10][:10]
 
         # Parse JSON
 
+        json_raw = {"cards":[]}
+
+        for card in res:
+            if (card[0]["document_title_pdf"].isupper()):
+                title = card[0]["document_title_pdf"].title()
+            else:
+                title = card[0]["document_title_pdf"]
+
+            json_raw["cards"].append({
+                "title": title,
+                "type": card[0]["document_type"],
+                "url": card[0]["document_url"],
+                "filename": card[0]["filename"],
+                "tags": [c[0].lower() for c in card[0]["keywords"][:5]],
+                "entities": card[0]["entities"][:5],
+                "summary": card[0]["document_summary"],
+                "thumbnail": card[0]["thumbnail_path"],
+                "images": card[0]["extracted_image_paths"],
+                "date": card[0]["date"],
+                "lingvector": card[0]["lingvector"]
+            })
+
+
         # Send response as JSON
         s.send_response(200)
-        s.send_header("Content-type", "text/html")
+        s.send_header("Content-type", "text/json")
+        s.send_header("Access-Control-Allow-Origin", "*")
         s.end_headers()
 
         # Write JSON
-        s.wfile.write(b"<html><head><title>Title goes here.</title></head>")
-        s.wfile.write(b"<body><p>This is a test.</p>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
-        s.wfile.write(b"</body></html>")
+        s.wfile.write(bytes(json.dumps(json_raw), 'utf-8'))
 
 def run(server_class=HTTPServer):
     server_address = ('', 3000)
