e58b9052c0c08c96b4a3207c5a446ec43482dc4f
manu183 <manuellang183@gmail.com>
2018-04-22 04:27:13 +0200

    crossorigin

--- backend/webserver_flask.py
+++ backend/webserver_flask.py
@@ -10,7 +10,51 @@ from pymongo import MongoClient
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, KeywordsOptions, EntitiesOptions
 from flask import Flask, jsonify
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 app = Flask(__name__)
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def mongo_connect():
     client = MongoClient('localhost', 27017)
@@ -20,6 +64,7 @@ def mongo_connect():
     return documents
 
 @app.route('/<query>')
+@crossdomain(origin='*')
 def show_user_profile(query):
     t = query.replace('%20',' ')
 
